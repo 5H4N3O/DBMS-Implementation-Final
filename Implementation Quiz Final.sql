319e820144439228a691c85ffeda8b52f2b16cf5
5H4N3O <shanepratt2002@gmail.com>
2025-05-03 16:54:49 -0500

    Seperated title genre lists into seperate entries for each title and stored them in a seperate table

--- Implementation Quiz Final.sql
+++ Implementation Quiz Final.sql
@@ -7,3 +7,57 @@ select *
 from temptitlebasics
 where isadult = 0;
 
+/* ========== Fixing titleBasics genres ========== */
+alter table titleBasics
+add constraint titleBasics_pk primary key (tconst);
+
+create table genres (
+    genre varchar(30) primary key
+);
+
+insert into genres (genre) values
+('Action'),
+('Adult'),
+('Adventure'),
+('Animation'),
+('Biography'),
+('Comedy'),
+('Crime'),
+('Documentary'),
+('Drama'),
+('Family'),
+('Fantasy'),
+('Film-Noir'),
+('Game-Show'),
+('History'),
+('Horror'),
+('Music'),
+('Musical'),
+('Mystery'),
+('News'),
+('Reality-TV'),
+('Romance'),
+('Sci-Fi'),
+('Short'),
+('Sport'),
+('Talk-Show'),
+('Thriller'),
+('War'),
+('Western');
+
+create table titleGenres (
+    tconst varchar(12) not null,
+    genre varchar(30) not null,
+    primary key (tconst, genre),
+    foreign key (tconst) references titleBasics(tconst)
+);
+
+insert into titleGenres (tconst, genre)
+select tconst, trim(genre) as genre
+from (
+    select tconst, unnest(string_to_array(genres, ',')) as genre
+    from titleBasics
+    where genres is not null
+     ) as split_genres
+where genre <> '';
+/* ================================================================ */
