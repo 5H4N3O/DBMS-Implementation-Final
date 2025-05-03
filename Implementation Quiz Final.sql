6ad27de68da631c006e9bbb9e2ec98ff7f8d9679
5H4N3O <shanepratt2002@gmail.com>
2025-05-03 16:56:03 -0500

    Separated lists of primaryProfession and knownForTitles for the namesBasics table

--- Implementation Quiz Final.sql
+++ Implementation Quiz Final.sql
@@ -61,3 +61,94 @@ from (
      ) as split_genres
 where genre <> '';
 /* ================================================================ */
+
+/* === Fixing nameBasics primaryProfession and knownForTitles === */
+alter table nameBasics
+add constraint nameBasics_pk primary key (nconst);
+
+create table professions (
+    profession varchar(30) primary key
+);
+
+insert into professions (profession) values
+('accountant'),
+('actor'),
+('actress'),
+('animation_department'),
+('archive_footage'),
+('archive_sound'),
+('art_department'),
+('art_director'),
+('assistant'),
+('assistant_director'),
+('camera_department'),
+('casting_department'),
+('casting_director'),
+('choreographer'),
+('cinematographer'),
+('composer'),
+('costume_department'),
+('costume_designer'),
+('director'),
+('editor'),
+('editorial_department'),
+('electrical_department'),
+('executive'),
+('legal'),
+('location_management'),
+('make_up_department'),
+('manager'),
+('miscellaneous'),
+('music_artist'),
+('music_department'),
+('podcaster'),
+('producer'),
+('production_department'),
+('production_designer'),
+('production_manager'),
+('publicist'),
+('script_department'),
+('set_decorator'),
+('sound_department'),
+('soundtrack'),
+('special_effects'),
+('stunts'),
+('talent_agent'),
+('transportation_department'),
+('visual_effects'),
+('writer');
+
+create table nameProfessions (
+    nconst varchar(12) not null,
+    profession varchar(30) not null,
+    primary key (nconst, profession),
+    foreign key (nconst) references nameBasics(nconst)
+);
+
+INSERT INTO nameProfessions (nconst, profession)
+SELECT nconst, TRIM(profession) AS profession
+FROM (
+    SELECT nconst, unnest(string_to_array(primaryProfession, ',')) AS profession
+    FROM nameBasics
+    WHERE primaryProfession IS NOT NULL
+) AS split_professions
+WHERE profession <> '';
+
+create table nameKnownForTitles (
+    nconst varchar(12) not null,
+    knownForTitle varchar(12) not null,
+    primary key (nconst, knownForTitle),
+    foreign key (nconst) references nameBasics(nconst),
+    foreign key (knownForTitle) references titleBasics(tconst)
+);
+
+--Below here has NOT been run
+
+INSERT INTO nameKnownForTitles (nconst, knownForTitle)
+SELECT nconst, TRIM(knownForTitle) AS knownForTitle
+FROM (
+    SELECT nconst, unnest(string_to_array(knownForTitles, ',')) AS knownForTitle
+    FROM nameBasics
+    WHERE knownForTitles IS NOT NULL
+) AS split_titles, titleBasics b
+WHERE knownForTitle <> '' and b.tconst = knownForTitle;
