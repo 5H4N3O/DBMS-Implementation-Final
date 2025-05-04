949414638ff134083dadf6b72b8a413853c4544a
5H4N3O <shanepratt2002@gmail.com>
2025-05-03 23:27:04 -0500

    Create Query 3.sql

---
+++ Query 3.sql
@@ -0,0 +1,26 @@
+/*
+ Determine the top 5 actors, writers, or producers who have worked on
+ the same movie as Kevin Bacon by how many times they worked on movies
+ with Kevin Bacon (excluding Kevin Bacon himself).
+ */
+create temp table baconMovies as
+select tconst
+from titlePrincipals p
+join nameBasics b on p.nconst = b.nconst
+where b.primaryname = 'Kevin Bacon';
+
+create temp table eligiblePeople as
+select distinct p.nconst
+from baconMovies b
+join titlePrincipals p on b.tconst = p.tconst
+where (p.category = 'actor' or p.category = 'writer' or p.category = 'producer')
+  and p.nconst not in (select nconst from nameBasics where primaryname = 'Kevin Bacon');
+
+select n.primaryname, count(*) as baconMovies
+from titleprincipals p
+    join eligiblePeople e on p.nconst = e.nconst
+    join nameBasics n on n.nconst = e.nconst
+    join baconMovies b on b.tconst = p.tconst
+group by n.primaryname
+order by baconMovies desc
+limit 5;
